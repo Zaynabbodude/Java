735d850a82da51c7dad9a58006ebe0cfc800c3af
zaynabbodude <zainabbodude1@gmail.com>
2024-01-03 07:36:02 +0100

    java

---
+++ Linear/Age.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+    public class Age{
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter your age: " );
+            int age = input.nextInt();
+            System.out.println("Enter current year: " );
+            int year = input.nextInt();
+
+            int yearBorn = year - age ;
+            System.out.println("You were born in the year: " + yearBorn);
+        }
+    }
